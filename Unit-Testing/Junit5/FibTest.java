7e72aad1c7815bac7d58ea336b0f998eaf006e28
Murtaza <42022929+murtazamnm@users.noreply.github.com>
2023-01-18 00:44:57 +0100

    Update and rename fibTest.java to FibTest.java

--- Unit-Testing/Junit5/FibTest.java
+++ Unit-Testing/Junit5/FibTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-class fibTest {
+class FibTest {
 
 	@Test
 	void testIsZero() {
