9291291a07b2bd9ebd3aa24fea4afd327c686878
Murtaza <42022929+murtazamnm@users.noreply.github.com>
2023-01-18 00:38:33 +0100

    Create fib.java

---
+++ Unit-Testing/Junit5/fib.java
@@ -0,0 +1,20 @@
+package testingg;
+
+import java.util.Iterator;
+
+public class fib {
+	public static int fib(int n){
+		if (n < 0) throw new IllegalArgumentException("Negative...");
+		if(n==0) return 1;
+		if(n == 1) return 1;
+		return fib(n-1)+fib(n-2);
+	}
+	
+	public static int[] fibSereies(int n){
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = fib(i);
+		}
+		return result;
+	}
+}
